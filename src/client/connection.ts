b0d7397da41c0e952ed0c734f6d19ba75b3381ea
nvms <pyersjonathan@gmail.com>
2022-11-22 08:54:30 -0700

    Comments and types for events.

--- src/client/connection.ts
+++ src/client/connection.ts
@@ -5,19 +5,43 @@ type Command = {
   id?: number;
   command: string;
   payload?: any;
-}
+};
+
+type LatencyPayload = {
+  /** Round trip time in milliseconds. */
+  payload: number;
+};
 
 export declare interface Connection extends EventTarget {
   addEventListener(type: "message", listener: (ev: CustomEvent) => any, options?: boolean | AddEventListenerOptions): void;
+
+  /** Emits when a connection is made. */
   addEventListener(type: "connection", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+  /** Emits when a connection is made. */
+  addEventListener(type: "connected", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+  /** Emits when a connection is made. */
+  addEventListener(type: "connect", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+
+  /** Emits when a connection is closed. */
   addEventListener(type: "close", listener: () => any, options?: boolean | AddEventListenerOptions): void;
-  addEventListener(type: "ping", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+  /** Emits when a connection is closed. */
+  addEventListener(type: "closed", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+  /** Emits when a connection is closed. */
+  addEventListener(type: "disconnect", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+  /** Emits when a connection is closed. */
+  addEventListener(type: "disconnected", listener: () => any, options?: boolean | AddEventListenerOptions): void;
 
   /** Emits when a reconnect event is successful. */
   addEventListener(type: "reconnect", listener: () => any, options?: boolean | AddEventListenerOptions): void;
 
   /** Emits when a reconnect fails after @see KeepAliveClientOptions.maxReconnectAttempts attempts. */
   addEventListener(type: "reconnectfailed", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+
+  /** Emits when a ping message is received from @see KeepAliveServer from `@prsm/keepalive-ws/server`. */
+  addEventListener(type: "ping", listener: () => any, options?: boolean | AddEventListenerOptions): void;
+
+  /** Emits when a latency event is received from @see KeepAliveServer from `@prsm/keepalive-ws/server`. */
+  addEventListener(type: "latency", listener: (ev: CustomEventInit<LatencyPayload>) => any, options?: boolean | AddEventListenerOptions): void;
 
   addEventListener(type: string, listener: (ev: CustomEvent) => any, options?: boolean | AddEventListenerOptions): void;
 }
